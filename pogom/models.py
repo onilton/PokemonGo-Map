7ee29b92ba66c1679dcc24c1ac061ee3262dd453
kevdotcom <kevinschrader@gmx.de>
2017-01-22 05:16:14 +0100

    change get_match to get tokens max 30 seconds old

--- pogom/models.py
+++ pogom/models.py
@@ -1568,7 +1568,7 @@ class Token(flaskDb.Model):
         with flaskDb.database.transaction():
             d_token = (Token
                        .select()
-                       .where(Token.last_updated >= request_time)
+                       .where(Token.last_updated >= datetime.utcnow() - timedelta(seconds=30))
                        .order_by(Token.last_updated)
                        .first())
             if d_token is not None:
